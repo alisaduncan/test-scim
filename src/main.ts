09929a7ff0a3f929b8888a33fe43b9fda3e406ae
Alisa <7586237+alisaduncan@users.noreply.github.com>
2024-06-11 06:24:07 -0700

    uncomment remaining initial code

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 // import { PrismaClient, Todo, User } from '@prisma/client';
-// import passport from 'passport';
-// import passportBearer from 'passport-http-bearer';
+import passport from 'passport';
+import passportBearer from 'passport-http-bearer';
 
 
 // body-parser is required to accept the header content-type application/scim+json from Okta
 // https://www.npmjs.com/package/body-parser
 // RFC Notes: https://datatracker.ietf.org/doc/html/rfc7644#section-3.1
-// import bodyParser from 'body-parser';
+import bodyParser from 'body-parser';
 
 // For logging http requests
 import morgan from 'morgan';
@@ -17,28 +17,28 @@ import { scimRoute } from './scim';
 
 
 // const prisma = new PrismaClient();
-// const BearerStrategy = passportBearer.Strategy;
+const BearerStrategy = passportBearer.Strategy;
 
 const app = express();
 app.use(express.json())
 
-// app.use(passport.initialize());
+app.use(passport.initialize());
 
-// app.get('/', (req, res) => {
-//   res.send('Hello, TypeScript Express!');
-// });
+app.get('/', (req, res) => {
+  res.send('Hello, TypeScript Express!');
+});
 
 ///////////////////////////////////////////////////////
 // SCIM-related routes
 
-// passport.use(new BearerStrategy(
-//   async (apikey, done) => {
+passport.use(new BearerStrategy(
+  async (apikey, done) => {
 
-//     return done(null);
-//   }
-// ));
+    return done(null);
+  }
+));
 
-// app.use(bodyParser.json({ type: 'application/scim+json' }));
+app.use(bodyParser.json({ type: 'application/scim+json' }));
 
 app.use(morgan('combined'))
 
