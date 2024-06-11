d03eac5cac712863c669c64a7edfaea85041e22e
Alisa <7586237+alisaduncan@users.noreply.github.com>
2024-06-10 18:17:14 -0700

    test

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,6 @@ import express from 'express';
 // import passport from 'passport';
 // import passportBearer from 'passport-http-bearer';
 
-const app = express();
-const port = 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello CodeSandbox!");
-});
-
-app.listen(port, () => {
-  console.log(`Sandbox listening on port ${port}`);
-});
 
 // body-parser is required to accept the header content-type application/scim+json from Okta
 // https://www.npmjs.com/package/body-parser
@@ -20,17 +10,17 @@ app.listen(port, () => {
 // import bodyParser from 'body-parser';
 
 // For logging http requests
-// import morgan from 'morgan';
+import morgan from 'morgan';
 
 // Import the scimRoute from the scim.ts file
-// import { scimRoute } from './scim';
+import { scimRoute } from './scim';
 
 
 // const prisma = new PrismaClient();
 // const BearerStrategy = passportBearer.Strategy;
 
-// const app = express();
-// app.use(express.json())
+const app = express();
+app.use(express.json())
 
 // app.use(passport.initialize());
 
@@ -50,14 +40,14 @@ app.listen(port, () => {
 
 // app.use(bodyParser.json({ type: 'application/scim+json' }));
 
-// app.use(morgan('combined'))
+app.use(morgan('combined'))
 
 // '/scim/v2' path appends to every SCIM Endpoints
 // Okta recommended url - https://developer.okta.com/docs/guides/scim-provisioning-integration-prepare/main/#base-url
-// app.use('/scim/v2', scimRoute);
+app.use('/scim/v2', scimRoute);
 
-// const port = process.env.PORT || 3333;
-// const server = app.listen(port, () => {
-//   console.log(`Listening at http://localhost:${port}/scim/v2`);
-// });
-// server.on('error', console.error);
+const port = process.env.PORT || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/scim/v2`);
+});
+server.on('error', console.error);
